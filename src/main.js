8361525954e6bf453f0996df03062f2939d86e3b
brooke perry and ryan leslie <brooke+ryan@>
2019-01-15 16:33:47 -0800

    make api for giphy

--- src/main.js
+++ src/main.js
@@ -11,14 +11,11 @@ $(document).ready(function() {
   $(".attack").hide();
   let bear = new HungryBear();
 
-
   $("#startForm").click(function(event) {
     event.preventDefault();
     bear.setHunger();
     bear.setSleepy();
     bear.setPoke();
-
-
     console.log(bear)
 
   });
@@ -45,19 +42,37 @@ $(document).ready(function() {
       }
     }, 250)
     $('#testy').hide();
-
-
   });
-
-
   $('#bearForm').click(function(event){
     event.preventDefault();
     $("#feed").click(bear.feed());
     $("#sleep").click(bear.sleep());
     $("#poke").click(bear.poke());
-
-
-
   });
+
+    $('#weatherLocation').click(function() {
+    const species = $('#input-species').val();
+    $('#input-species').val("");
+
+    let request = new XMLHttpRequest();
+    const url = `http://api.giphy.com/v1/gifs/search?q=${species}&api_key=${process.env.GIPHY_KEY}&limit=5`;
+
+    request.onreadystatechange = function() {
+      if (this.readyState === 4 && this.status === 200) {
+        const response = JSON.parse(this.responseText);
+        getElements(response);
+      }
+    }
+
+    request.open("GET", url, true);
+    request.send();
+
+   const getElements = function(response) {
+     // let body = JSON.parse(response);
+      $('#showAnimal').html(`<img src="${response.data[2].images.fixed_height.url}">`);
+      // $('.showTemp').text(`The temperature in Kelvins is ${response.img} degrees.`);
+    }
+  });
+  // });
 
 });
